482461519613492163d93dcfabf02f82e7b5128b
Jacky Liang <jackyliang@berkeley.edu>
2014-10-05 09:14:51 -0700

    -changed FB setting to production -added FB meta

--- app/stats/stats.js
+++ app/stats/stats.js
@@ -39,7 +39,7 @@ angular.module("teluxe")
             FB.ui(
                 {
                     method: 'share',
-                    href: 'teluxe.ngrok.com',
+                    href: 'teluxe.me',
                     caption:$scope.shareText
                 },
                 function(response) {
