562bb0c217e473c026925650e442288ee1848e21
JonChu <jonmchu@berkeley.edu>
2014-10-04 18:06:31 -0700

    search for light bulbs

--- app/services/ebay_search.js
+++ app/services/ebay_search.js
@@ -5,8 +5,8 @@
 angular.module("teluxe")
     .factory('ebay_search_service',function($http){
         return {
-            getLightBulbs: function(watt, temp){
-                lightType = watt + ""
+            getLightBulbs: function(watt, bulb){
+                lightType = "GE"+"%20" + watt+"W" +"%20" + bulb + "%20light%20bulb";
                 delete $http.defaults.headers.common['X-Requested-With'];
                 return $http.get("http://www.corsproxy.com/svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.0.0" + "&SECURITY-APPNAME=Teluxe353-437d-47df-922a-5d92b1cc138&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&keywords="+lightType);
             }
--- app/test/ebay.js
+++ app/test/ebay.js
@@ -11,12 +11,21 @@ angular.module("teluxe")
         $scope.setLightsFromData = function(data){
 
             for(var i = 0; i<5; i++){
+                //Selects the URL to image posted on Ebay
                 light_galleryUrl = data['findItemsByKeywordsResponse']['0']['searchResult']['0']['item'][i]['galleryURL']['0'];
+
+                //Selects Title of Product
                 light_title = data['findItemsByKeywordsResponse']['0']['searchResult']['0']['item'][i]['title']['0'];
+
+                //Gives url to specified item
                 light_url = data['findItemsByKeywordsResponse']['0']['searchResult']['0']['item'][i]['viewItemURL']['0'];
+
+                //Gives price in seller's specified currency
                 light_currentPriceValue = data['findItemsByKeywordsResponse']['0']['searchResult']['0']['item'][i]['sellingStatus']['0']['currentPrice']['0']['__value__'];
                 light_currentPriceCurrency = data['findItemsByKeywordsResponse']['0']['searchResult']['0']['item'][i]['sellingStatus']['0']['currentPrice']['0']['@currencyId'];
                 light_currentPrice = light_currentPriceValue +" "+ light_currentPriceCurrency;
+
+                //Time before end of sale
                 light_timeLeft = data['findItemsByKeywordsResponse']['0']['searchResult']['0']['item'][i]['sellingStatus']['0']['timeLeft']['0'];
                 light_daysLeft = light_timeLeft.substring(light_timeLeft.indexOf("P")+1, light_timeLeft.indexOf("D")) + " days";
                 light_hoursLeft = light_timeLeft.substring(light_timeLeft.indexOf("T")+1, light_timeLeft.indexOf("H")) + " hours";
@@ -28,13 +37,13 @@ angular.module("teluxe")
                     light_time = light_daysLeft;
                 }
 
-
+                //Stores info taken from Ebay into Array
                 $scope.lights.push([light_galleryUrl, light_title, light_url, light_currentPrice, light_time])
             }
             return $scope.lights;
         };
 
-        ebay_search_service.getLightBulbs()
+        ebay_search_service.getLightBulbs(40, "flourescent")
             .success(function(data, status, headers, config){
                 console.log(data);
                 console.log($scope.setLightsFromData(data));
